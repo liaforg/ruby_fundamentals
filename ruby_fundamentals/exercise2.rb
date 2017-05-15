7a5edaca78c6b0366e45e81b1912a5866a5758f0
Lia Forgione <liaforg@gmail.com>
2017-05-15 13:13:50 -0400

    ruby exercises 1 -3

---
+++ ruby_fundamentals/exercise2.rb
@@ -0,0 +1,22 @@
+# 1
+
+puts (55.0 * 0.15)
+
+#2
+
+puts "Great tip is #{55.0 * 0.15}"
+
+puts "Great tip is #{55.0 * 0.15 + 1}"
+
+#3
+
+puts "The output of 45628 times 7839 is #{45628 * 7839}"
+
+#4
+
+puts (10<20 && 30<20) #false
+puts !(10==11) #true
+
+#therefore below is true
+
+puts (10<20 && 30 < 20) || !(10==11)
